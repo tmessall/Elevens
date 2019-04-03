333da206db8a1531f82ab71c1bf59d41b2368092
Travis Thomas Messall <messalltt@s.dcsdk12.org>
2019-04-03 08:22:35 -0600

    Complete exercise 1 (implement the shuffle method into Deck)

--- src/Deck.java
+++ src/Deck.java
@@ -62,7 +62,12 @@ public class Deck {
 	 * and reset the size to represent the entire deck.
 	 */
 	public void shuffle() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+		for (int k = cards.size() - 1; k >= 1; k--) {
+			int r = (int) (Math.random() * k);
+			Card temp = cards.get(r);
+			cards.set(r, cards.get(k));
+			cards.set(k, temp);
+		}
 	}
 
 	/**
